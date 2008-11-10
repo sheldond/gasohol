66befcce651512ba6e748e43bc6c4f0b8356d4c8
Rob Cameron <cannikinn@gmail.com>
2008-11-10 14:04:01 -0800

    Added zips back in, moved default data to /migrate directory instead of root

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20080618144803) do
+ActiveRecord::Schema.define(:version => 20080618144804) do
 
   create_table "sessions", :force => true do |t|
     t.string   "session_id",                     :null => false
@@ -35,9 +35,27 @@ ActiveRecord::Schema.define(:version => 20080618144803) do
     t.boolean  "is_admin",      :default => false
   end
 
-  create_table "zips", :force => true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "zips", :id => false, :force => true do |t|
+    t.string  "city",                                        :default => "NULL"
+    t.string  "state",                                       :default => "NULL"
+    t.integer "zip",                                         :default => 0
+    t.integer "area_code",                                   :default => 0
+    t.integer "fips",                                        :default => 0
+    t.string  "county",                                      :default => "NULL"
+    t.string  "preferred",                                   :default => "NULL"
+    t.string  "time_zone",                                   :default => "NULL"
+    t.boolean "dst",                                         :default => false
+    t.decimal "latitude",      :precision => 7, :scale => 5, :default => 0.0
+    t.decimal "longitude",     :precision => 7, :scale => 5, :default => 0.0
+    t.integer "msa",                                         :default => 0
+    t.integer "pmsa",                                        :default => 0
+    t.integer "congress_dist",                               :default => 0
+    t.integer "dma",                                         :default => 0
+    t.string  "type",                                        :default => "NULL"
+    t.integer "batch",                                       :default => 0
+    t.integer "status",                                      :default => 0
   end
+
+  add_index "zips", ["zip"], :name => "index_zips_on_zip", :unique => true
 
 end
